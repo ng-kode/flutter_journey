fda60a8ea120e5a654ef88cc5fff71e9c183c958
Richard-NG <richard.chihong.ng@gmail.com>
2018-07-25 00:42:16 +0800

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,17 @@
+// import helper library
+import 'package:flutter/material.dart';
+
+// Define a main function
+void main() {
+  // Create a text widget
+  var app = MaterialApp(
+    home: Text('hi there')
+  );
+
+  // Take that widget to the screen
+  runApp(app);
+}
+
+
+
+
