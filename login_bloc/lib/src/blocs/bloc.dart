36b912e094bd83fa437e78208f16e789c01059a8
Richard Ng <richard.ng@corp.agscreative.com>
2018-07-26 13:12:59 +0800

    rxdart to combine streams

--- login_bloc/lib/src/blocs/bloc.dart
+++ login_bloc/lib/src/blocs/bloc.dart
@@ -1,9 +1,10 @@
 import 'dart:async';
+import 'package:rxdart/rxdart.dart';
 import 'validators.dart';
 
 class Bloc extends Object with Validators {
-  final _emailCrtl = StreamController<String>();
-  final _passwordCrtl = StreamController<String>();
+  final _emailCrtl = StreamController<String>.broadcast();
+  final _passwordCrtl = StreamController<String>.broadcast();
 
   // Access to stream
   Stream<String> get email => _emailCrtl
@@ -12,6 +13,8 @@ class Bloc extends Object with Validators {
   Stream<String> get password => _passwordCrtl
     .stream
     .transform(validatePassword);
+  Stream<bool> get submitValid => Observable
+    .combineLatest2(email, password, (eData, pData) => true);
 
   // Add data to stream
   Function(String) get changeEmail => _emailCrtl.sink.add;
@@ -20,5 +23,9 @@ class Bloc extends Object with Validators {
   dispose() {
     _emailCrtl.close();
     _passwordCrtl.close();
+  }
+
+  submit() {
+    
   }
 }
