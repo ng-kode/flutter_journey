fad015c3f1afd6ca4710e5746c10fe6ae64bc79a
Richard Ng <richard.ng@agscreative.com>
2018-07-26 18:05:19 +0800

    first animation

--- animation/lib/src/screens/home.dart
+++ animation/lib/src/screens/home.dart
@@ -5,7 +5,25 @@ class Home extends StatefulWidget {
   State<StatefulWidget> createState() => HomeState();
 }
 
-class HomeState extends State<Home> {
+class HomeState extends State<Home> with TickerProviderStateMixin {
+  Animation<double> catAnimation;
+  AnimationController catController;
+
+  initState() {
+    super.initState();
+    catController = AnimationController(
+      duration: Duration(seconds: 2),
+      vsync: this,
+    );
+    catAnimation = Tween(begin: 0.0, end: 100.0).animate(
+      CurvedAnimation(
+        parent: catController,
+        curve: Curves.easeIn,
+      ) // CurvedAnimation
+    ); // animate
+    catController.forward();
+  }
+
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
@@ -16,6 +34,15 @@ class HomeState extends State<Home> {
   }
 
   buildAnimation() {
-    return Cat();
+    return AnimatedBuilder(
+      animation: catAnimation,
+      builder: (context, child) {
+        return Container(
+          child: child,
+          margin: EdgeInsets.only(top: catAnimation.value),
+        );
+      },
+      child: Cat(),
+    );
   }
 }
