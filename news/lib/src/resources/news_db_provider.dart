973b14cf0c07a71cfb08a0eeb1d1cd423e736f4f
Richard-NG <richard.chihong.ng@gmail.com>
2018-07-27 23:56:05 +0800

    NewsDbProvider; No test for sqflite

---
+++ news/lib/src/resources/news_db_provider.dart
@@ -0,0 +1,65 @@
+import 'package:sqflite/sqflite.dart';
+import 'package:path_provider/path_provider.dart';
+import 'dart:io';
+import 'package:path/path.dart';
+import 'dart:async';
+import '../models/item_model.dart';
+
+class NewsDbProvider {
+  Database db;
+
+  init() async {
+    // return the location of directory for app storage
+    Directory documentsDirectory = await getApplicationDocumentsDirectory();
+
+    // join path to point to the db file
+    final path = join(documentsDirectory.path, "items.db");
+
+    // if path exists, open the db
+    // else, create one
+    db = await openDatabase(
+      path,
+      version: 1,
+      onCreate: (Database newDb, int version) {
+        newDb.execute("""
+          CREATE TABLE Items
+          (
+            id INTEGER PRIMARY KEY,
+            type TEXT,
+            by TEXT,
+            time INTEGER,
+            text TEXT,
+            dead INTEGER,
+            parent INTEGER,
+            kids BLOB,
+            deleted INTEGER,
+            url TEXT,
+            score INTEGER,
+            title TEXT,
+            descendants INTEGER
+          )
+        """);
+      }
+    );
+  }
+
+  fetchItem(int id) async {
+    // maps ~ array of objects
+    final maps = await db.query(
+      "Items",
+      columns: null,
+      where: "id = ?",
+      whereArgs: [id], // whereArgs protect us from SQL injection attack
+    );
+
+    if (maps.length > 0) {
+      return ItemModel.fromDb(maps.first);
+    }
+
+    return null;
+  }
+
+  addItem(ItemModel item) {
+    db.insert("Items", item.toMapForDb());
+  }
+}
